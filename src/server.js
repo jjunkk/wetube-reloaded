c43925436d0a4017656b365e21bae06cb67ccfd5
jjunkk <junkyoung.lee@gmail.com>
2022-03-07 17:12:00 +0900

    router

--- src/server.js
+++ src/server.js
@@ -5,19 +5,23 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  return res.send("hello");
-};
-
-const login = (req, res) => {
-  return res.send("login");
-};
-
 app.use(logger);
 
-app.get("/", home);
-app.get("/login", login);
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
