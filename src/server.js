d9b6dded69381d1d3fb20a3284236c190ff69bc8
jjunkk <junkyoung.lee@gmail.com>
2022-03-07 22:53:05 +0900

    add controllers folders

--- src/server.js
+++ src/server.js
@@ -1,23 +1,14 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 app.use(logger);
-
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
